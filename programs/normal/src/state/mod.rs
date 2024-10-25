228bd68f1df6e141eab7d989a7dbbff5a319a75e
Joshua Blew <joshua@normalfinance.io>
2024-10-25 11:32:55 -0500

    chore: remove spot fulfillment params from mod

--- programs/normal/src/state/mod.rs
+++ programs/normal/src/state/mod.rs
@@ -8,8 +8,6 @@ pub mod oracle;
 pub mod oracle_map;
 pub mod order_params;
 pub mod paused_operations;
-// pub mod settle_pnl_mode;
-pub mod spot_fulfillment_params;
 pub mod market;
 pub mod market_map;
 #[allow(clippy::module_inception)]
